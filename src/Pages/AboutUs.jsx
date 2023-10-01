a5524d9f6a824751efb142c9293c6126e538e149
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2023-10-02 00:56:08 +0530

    About Us Page responsive

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -8,13 +8,13 @@ function AboutUs(){
     
     return (
         <HomeLayout>
-            <div className="pl-20 pt-20 flex flex-col text-white">
-                <div className="flex items-center gap-5 mx-10">
-                    <section className="w-1/2 space-y-10">
-                        <h1 className="text-5xl text-yellow-500 font-semibold">
+            <div className="sm:pl-20 pt-20 flex flex-col text-white">
+                <div className="flex flex-col sm:flex-row items-center gap-5 mx-4 sm:mx-10">
+                    <section className="w-full sm:w-1/2 space-y-10">
+                        <h1 className="text-4xl sm:text-5xl text-center sm:text-left text-yellow-500 font-semibold">
                             Affordable and quality education
                         </h1>
-                        <p className="text-xl text-gray-200">
+                        <p className="sm:text-xl text-center sm:text-left text-gray-200">
                             Out goal is to provide the affordabe and quality education to the world. 
                             We are providing the platform for the aspiring teachers and students to 
                             share their skills, creativity and knowledge with each other to empower 
@@ -22,7 +22,7 @@ function AboutUs(){
                         </p>
                     </section>
 
-                    <div className="w-1/2">
+                    <div className="w-full sm:w-1/2 ">
                         <img 
                             id="test1" 
                             style={{filter: "drop-shadow(10px 10px 5px rgb(0,0,0))"}} 
@@ -32,7 +32,7 @@ function AboutUs(){
                     </div>
                 </div>
 
-                <div className="carousel w-1/2 my-16 m-auto">
+                <div className="carousel w-full sm:w-1/2 my-8 sm:my-16 m-auto">
                     {persons && persons.map(item  => 
                         <CarouselSlide {...item} key={item.slideNumber} totalSlides={persons.length} />
                     )}
